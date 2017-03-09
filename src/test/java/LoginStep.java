c08909fa4f55e91ab640f7c99b724e6a86fc395a
Rosa Roman Alvarez <rosroman@esmadlbi0095.iconib.local>
2017-03-09 16:01:43 +0100

    First commit

---
+++ src/test/java/LoginStep.java
@@ -0,0 +1,33 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+/**
+ * Created by rosroman on 09/03/17.
+ */
+public class LoginStep {
+    @And("^I click login button$")
+    public void iClickLoginButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I navigate to the login page$")
+    public void iNavigateToTheLoginPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter the username as admin and password as admin$")
+    public void iEnterTheUsernameAsAdminAndPasswordAsAdmin() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see the userform page$")
+    public void iShouldSeeTheUserformPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
